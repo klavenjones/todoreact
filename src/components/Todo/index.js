72f2edd8ae4d48264a4f73add0a3e4d37a8b60d0
klavenjones <klavensjones@gmail.com>
2021-09-01 17:09:50 -0400

    Finished base todo design

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,12 +1,89 @@
-import React from 'react'
+import { useState } from 'react'
+import { FaSearch, FaPlus, FaPen, FaTrash } from 'react-icons/fa'
 import { useUser } from '../../context/useUser'
 
-export function Todo() {
-  const { logout } = useUser()
+import Navigation from './navigation'
+
+function AddTodoForm() {
   return (
-    <div className='container'>
-      <h1>Logged in</h1>
-      <button onClick={logout}>Logout</button>
-    </div>
+    <>
+      <div className='add'>
+        <div className='input-group'>
+          <FaPlus className='icon' />
+          <input type='text' placeholder='Add New Item' />
+        </div>
+        <div>
+          <button>Save</button>
+        </div>
+      </div>
+    </>
+  )
+}
+function EditTodoForm({ saveTodo }) {
+  return (
+    <>
+      <li>
+        <div className='input-group'>
+          <FaPen className='icon' />
+          <input type='text' placeholder='Edit Item' />
+        </div>
+        <div>
+          <button onClick={() => saveTodo()}>Save</button>
+        </div>
+      </li>
+    </>
+  )
+}
+
+export function Todo() {
+  const [editTodo, setEdit] = useState(false)
+  const [newTodo, setNew] = useState(false)
+
+  const handleSave = () => {
+    setEdit(!editTodo)
+  }
+
+  return (
+    <>
+      <Navigation />
+      <h2>My To-Do List</h2>
+      <div className='container todo'>
+        <div className='todo__list'>
+          <div className='header'>
+            <div className='input-group'>
+              <FaSearch className='icon' />
+              <input type='text' placeholder='Search' />
+            </div>
+            <div>
+              <button onClick={() => setNew(!newTodo)}>New Todo</button>
+            </div>
+          </div>
+          {newTodo && <AddTodoForm type='add' />}
+          <div className='items'>
+            <ul>
+              {editTodo ? (
+                <EditTodoForm saveTodo={handleSave} />
+              ) : (
+                <li>
+                  <div className='item__name'>
+                    <p>TODO NAME</p>
+                  </div>
+                  <div className='item_buttons'>
+                    <FaPen
+                      className='icon'
+                      onClick={() => setEdit(!editTodo)}
+                    />
+                    <FaTrash
+                      className='icon'
+                      onClick={() => setEdit(!editTodo)}
+                    />
+                  </div>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
---
+++ src/components/Todo/navigation.js
@@ -0,0 +1,14 @@
+import { useUser } from '../../context/useUser'
+
+export default function Navigation() {
+  const { logout, authUser } = useUser()
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar__nav'>
+        <p>{authUser.email}</p>
+        <button onClick={logout}>Logout</button>
+      </div>
+    </nav>
+  )
+}
