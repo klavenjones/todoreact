083c16f3a59d234bd57007a468ea80f692ca39ef
klavenjones <klavensjones@gmail.com>
2021-08-31 23:42:55 -0400

    Added Error validation

--- src/App.js
+++ src/App.js
@@ -1,69 +1,133 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
 import { FaUserAlt, FaLock } from 'react-icons/fa'
 import './styles/main.scss'
+import { isCompositeComponentWithType } from 'react-dom/test-utils'
 
 function App() {
-  const [errors, setErrors] = useState({ email: '', password: '' })
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful, isSubmitting, isValid },
+    handleSubmit
+  } = useForm({
+    mode: 'all',
+    reValidateMode: 'onChange'
+  })
 
-  function emailIsValid(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  const [disabled, setDisabled] = useState(false)
+
+  const onSubmit = async (data, e) => {
+    if (isSubmitting) {
+      setDisabled(true)
+    }
+    try {
+      const response = await fetch(
+        'http://dev.rapptrlabs.com/Tests/scripts/user-login.php',
+        {
+          method: 'POST',
+          mode: 'cors',
+          body: JSON.stringify(data)
+        }
+      )
+      console.log(response.json())
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    let error = {}
-    if (!email || !emailIsValid(email)) {
-      error.email = 'Email is not valid'
+  //Checking if valid. If not we will disable the button.
+  useEffect(() => {
+    if (!isValid) {
+      setDisabled(true)
+    } else {
+      setDisabled(false)
     }
-
-    if (!password || password.length < 4) {
-      error.password = 'Password is not valid'
-    }
-    setErrors({ ...error, ...errors })
-  }
+    console.log(isValid)
+  }, [isValid])
 
   return (
     <main>
       <div className='container'>
         <h1>Rapptr Labs</h1>
-        <form className='login' onSubmit={handleSubmit}>
-          <div className='login__input'>
+        <form className='login' onSubmit={handleSubmit(onSubmit)}>
+          <div className='login__input '>
             <label htmlFor='email'>Email</label>
-            <div className='input-group'>
+            <div
+              className={`input-group ${errors.email && 'input-group--error'}`}
+            >
               <FaUserAlt className='icon' />
               <input
+                {...register('email', {
+                  required: true,
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: 'This not a valid email'
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: 'Email should not exceed 50 characters'
+                  }
+                })}
                 type='text'
-                name='email'
                 id='email'
                 placeholder='user@rapptrlabs.com'
-                value={email}
-                onFocus={() => setErrors({})}
-                onChange={(e) => {
-                  setEmail(e.target.value)
-                }}
               />
             </div>
-            {errors.email && <span>{errors.email}</span>}
+            {errors.email?.type === 'required' && (
+              <span>Email is required</span>
+            )}
+            {errors.email?.type === 'manual' && (
+              <span>{errors.email.message}</span>
+            )}
+            {errors.email?.type === 'pattern' && (
+              <span>{errors.email.message}</span>
+            )}
+            {errors.email?.type === 'maxLength' && (
+              <span>{errors.email.message}</span>
+            )}
           </div>
           <div className='login__input'>
             <label htmlFor='password'>Password</label>
-            <div className='input-group'>
+            <div
+              className={`input-group ${
+                errors.password && 'input-group--error'
+              }`}
+            >
               <FaLock className='icon' />
               <input
+                {...register('password', {
+                  required: true,
+                  minLength: {
+                    value: 4,
+                    message: 'Password should atleast be 4 characters'
+                  },
+                  maxLength: {
+                    value: 16,
+                    message: 'Password should not exceed 16 characters'
+                  }
+                })}
                 type='password'
-                name='password'
                 id='password'
                 placeholder='Must be at least 4 characters'
-                onFocus={() => setErrors({})}
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
               />
             </div>
-            {errors.password && <span>{errors.password}</span>}
+            {errors.password?.type === 'required' && (
+              <span>Password is required</span>
+            )}
+            {errors.password?.type === 'manual' && (
+              <span>{errors.password.message}</span>
+            )}
+            {errors.password?.type === 'minLength' && (
+              <span>{errors.password.message}</span>
+            )}
+            {errors.password?.type === 'maxLength' && (
+              <span>{errors.password.message}</span>
+            )}
           </div>
-          <button type='submit'>Login</button>
+          <button type='submit' disabled={disabled}>
+            Login
+          </button>
           {errors.server && (
             <span>
               The server could not be reached. Please try again later.
